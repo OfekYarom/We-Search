a82d6162de9859617df9640143c6fe73e5ced7ec
OfekYarom <38701053+OfekYarom@users.noreply.github.com>
2018-07-28 17:13:21 +0000

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,6 +1,4 @@
 ###############################   Import   ##################################################################################################
-from __future__ import division
-import urllib
 import string
 import math
 import urllib
@@ -8,10 +6,7 @@ import urllib2
 import Tkinter as tk
 from Tkinter import *
 import wikipedia
-from HTMLParser import HTMLParser
 ###############################    TF - IDF   #################################################################################################
-global all_documents
-global tokenize
 def documents (doc_0):
     # get the text from wikipedia according to the user input
     # the function is getting the text for each word and for the all input
@@ -176,7 +171,6 @@ def text_normal(the_out):
     return complite
 
 ###############################   UI   #############################################################################################
-global textInput
 def raise_frame(frame): # function for raising pages
     frame.tkraise()
 def Search(): # set up and start the procses of the TF IDF
@@ -190,8 +184,8 @@ def Search(): # set up and start the procses of the TF IDF
     if error > 0:
         raise_frame(f2)# opens the next page to show the output
     else:
-        raise_frame(f4)
-    # what to do when Search button clicked    
+        raise_frame(f4)# if no output have found
+    # what to do when Search button clicked
     
 def Send(): # the algorithem for the radio buttons
     X = var.get()
@@ -210,9 +204,16 @@ f2 = Frame(root)
 f3 = Frame(root)
 f4 = Frame(root)
 # setting the pages size acording to the screen size
-x_cordnite = ((root.winfo_screenwidth()/2) - 500)
-y_cordnite = ((root.winfo_screenheight()/2) - 325)
-root.geometry(("%dx%d+%d+%d") %(1000,650,x_cordnite,y_cordnite))
+screenwidth = root.winfo_screenwidth()
+screenheight = root.winfo_screenheight()
+size_labels = (screenwidth*screenheight)/26200
+size_smalllabels = size_labels/2
+size_of_txt = size_labels/5
+alot_of_txt = size_labels/10
+size_of_entry = size_labels*1.5
+x_cordnite = ((screenwidth/2)-(screenwidth/2.6))
+y_cordnite = ((screenheight/2)-screenheight/2.6)
+root.geometry(("%dx%d+%d+%d") %((screenwidth/1.3),(screenheight/1.3),x_cordnite,y_cordnite))
 root.resizable(0, 0)
 root.title("Search Engine") # giving the window a name
 
@@ -220,23 +221,23 @@ for frame in (f1, f2, f3, f4):
     frame.grid(row=0, column=0, sticky='news')
 
 #page one - get Search Word
-lbl_0 = Label(f1, text = "Search Engine", font = ("Arial Bold",50))
+lbl_0 = Label(f1, text = "Search Engine", font = ("Arial Bold",size_labels))
 lbl_0.grid(column=1, row = 1, sticky=(W, E))
-lbl_1 = Label(f1, text = "Looking for info? Type it here and we will find it for you!", font = ("Arial Bold",28))
+lbl_1 = Label(f1, text = "Looking for info? Type it here and we will find it for you!", font = ("Arial Bold",size_smalllabels))
 lbl_1.grid(column=1, row = 2, sticky=(W, E))
-txt = Entry(f1, width=70)
-txt.grid(column = 1, row = 3, pady = 40)
+txt = Entry(f1, width=int((size_of_entry)))
+txt.grid(column = 1, row = 3, pady = size_smalllabels)
 txt.focus()
 btn = Button(f1, text="Search", command=Search)
 btn.grid(column=1, row=4)
 
 #page two - get Feedback
 var = IntVar()
-lbl_4 = Label(f2, text = "Search Engine", font = ("Arial Bold",50))
+lbl_4 = Label(f2, text = "Search Engine", font = ("Arial Bold",size_labels))
 lbl_4.grid(column=1, row = 1, sticky=(W, E))
-lbl_10 = Label(f2, text = "txt", font = ("Arial Bold",10))
+lbl_10 = Label(f2, text = "txt", font = ("Arial Bold",size_of_txt))
 lbl_10.grid(column=1, row = 2, sticky=(W, E))
-lbl_2 = Label(f2, text = "Is this was helpful? do you want something better?", font = ("Arial Bold",31))
+lbl_2 = Label(f2, text = "Is this was helpful? Do you want something better?", font = ("Arial Bold",int(size_smalllabels*1.127)))
 lbl_2.grid(column=1, row = 5, sticky=(W, E))
 rad1 = Radiobutton(f2,text='Helpful',  variable=var, value=1)
 rad2 = Radiobutton(f2,text='Not Helpful',  variable=var, value=2)
@@ -250,49 +251,26 @@ btn = Button(f2, text="Send", command=Send)
 btn.grid(column=1, row=10)
 
 #page three - Last Messege
-lbl_5 = Label(f3, text = "Search Engine", font = ("Arial Bold",50))
+lbl_5 = Label(f3, text = "Search Engine", font = ("Arial Bold",size_labels))
 lbl_5.grid(column=1, row = 1, sticky=(W, E))
-lbl_6 = Label(f3, text = "Thank You For Helping Us Improve!", font = ("Arial Bold",44))
+lbl_6 = Label(f3, text = "Thank You For Helping Us Improve!", font = ("Arial Bold",int(size_labels/1.26)))
 lbl_6.grid(column=1, row = 2, sticky=(W, E))
-lbl_7 = Label(f3, text = "Please Come Back Agin For Better Answer", font = ("Arial Bold",20))
+lbl_7 = Label(f3, text = "Please Come Back Agin For Better Answer", font = ("Arial Bold",int(size_smalllabels*1.2)))
 lbl_7.grid(column=1, row = 3, sticky=(W, E))
 btn_11 = Button(f3, text="Try Agin", command=(lambda:raise_frame(f1)))
 btn_11.grid(column=1, row=10)
 
 #page Four - Last Messege
-lbl_12 = Label(f4, text = "Search Engine", font = ("Arial Bold",50))
+lbl_12 = Label(f4, text = "Search Engine", font = ("Arial Bold",size_labels))
 lbl_12.grid(column=1, row = 1, sticky=(W, E))
-lbl_13 = Label(f4, text = "Error Occurred", font = ("Arial Bold",44))
+lbl_13 = Label(f4, text = "Error Occurred", font = ("Arial Bold",size_labels))
 lbl_13.grid(column=1, row = 2, sticky=(W, E))
-lbl_14 = Label(f4, text = "Please Check Your Spelling And Try Agin", font = ("Arial Bold",20))
+lbl_14 = Label(f4, text = "Please Check Your Spelling Or Be More Specific And Try Agin", font = ("Arial Bold",int(size_smalllabels/1.07)))
 lbl_14.grid(column=1, row = 3, sticky=(W, E))
 btn_15 = Button(f4, text="Try Agin", command=(lambda:raise_frame(f1)))
 btn_15.grid(column=1, row=10)
 
 
-
-
-
-
-
-
-
-###############################   Strip Html Tags   ###########################################################################################
-class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset() # cleans the buffer and setting up
-        self.fed = []
-    def handle_data(self, l): # gets word and uppend it to a list
-        self.fed.append(l)
-    def handle_entityref(self, name):
-        self.fed.append('&%s;' % name)#using regex to clean the html out
-    def get_data(self): # getting all to words togheter
-        return ''.join(self.fed)
-
-def html_to_text(html): # building an object and activiting the class
-    s = MLStripper()
-    s.feed(html) # 'feeding' the class each word
-    return s.get_data()
 
 
 raise_frame(f1)# opening the first page - starts UI
